c661b2442436332c626d4dd1853f66badf6756df
Verum Lumen <rafa.munhoz@terra.com.br>
2018-03-22 10:14:32 -0300

    Update Exercicios3.hs

--- Atividade 01/Exercicios3.hs
+++ Atividade 01/Exercicios3.hs
@@ -8,13 +8,36 @@ ehTriangulo x y z = chLado x y z && chLado y x z && chLado z x y
 tipoTriangulo :: Double -> Double -> Double -> String
 
 tipoTriangulo x y z 
- | ehTriangulo x y z = "Nao Triangulo"
+ | not (ehTriangulo x y z) = "Nao Triangulo"
  | x == y && y == z  = "Triangulo Equilatero"
  | x == y || y == z || z == x  = "Triangulo Isosceles"
  | x /= y && y /= z && z /= x  = "Triangulo Escaleno"
 
 -- 03 Implemente uma função que faz a multiplicação etíope entre dois números.
+etiope :: Integer -> Integer -> Integer
+etiope m n =
 
+while( m!=0 ){   // m vai ser dividido sucessivamente por 2, até chegar em 0
+      if( m%2!=0 ){  // se m é par vou somar o valor de n correspondente
+        mult += n;
+      }
+      m /= 2;         // próxima linha da tabela, m/2 e n*2
+      n *= 2;
+    }
+
+
+-- 04 Faça uma função que determine se um número é primo.
+primo :: Int -> Int -> Bool
+primo n 1 = True
+primo n m = if (n `rem` m) == 0 then False else primo n (m-1)
+
+-- 05 Faça uma função que calcule a soma dos dígitos de um número.
+
+-- 06 Faça uma função que calcule a persistência aditiva de um número.
+
+-- 07 Faça uma função que calcule o coeficiente binomial de (m,n).
+
+-- 08 Faça uma função que calcule o elemento (i,j) do triângulo de pascal.
 
 -- Validação Simples
 main = do
@@ -30,4 +53,3 @@ main = do
   print (ex2b)
   print (ex2c)
   print (ex2d)
-
