b4ff4fffbe43811f0d88d11bc76ced9a5ea6dbaf
Verum Lumen <rafa.munhoz@terra.com.br>
2018-03-22 11:27:21 -0300

    Update Exercicios2.hs

--- Atividade 01/Exercicios2.hs
+++ Atividade 01/Exercicios2.hs
@@ -25,22 +25,43 @@ etiope m n
 
 
 -- 04 Faça uma função que determine se um número é primo.
-primo :: Int -> Int -> Bool
+primo :: Integer -> Integer -> Bool
 primo n m
  | m==1 = True
  |(n `rem` m) == 0 = False 
  | otherwise = primo n (m-1)
  
-ehPrimo :: Int -> Bool
+ehPrimo :: Integer -> Bool
 ehPrimo n = primo n (n-1)
 
 -- 05 Faça uma função que calcule a soma dos dígitos de um número.
+somaDigitos :: Integer -> Integer
+somaDigitos 0 = 0
+somaDigitos n =  (n `mod` 10) + somaDigitos(n `div` 10)
 
 -- 06 Faça uma função que calcule a persistência aditiva de um número.
+persAditiva :: Integer-> Integer
+persAditiva n
+  | n < 10 = 0
+  | otherwise = 1 + persAditiva (somaDigitos n)
 
 -- 07 Faça uma função que calcule o coeficiente binomial de (m,n).
+fatorial :: Integer -> Integer
+fatorial 0 = 1
+fatorial 1 = 1
+fatorial n = fatorial' n 1
+  where
+    fatorial' 1 r = r
+    fatorial' n r = fatorial' (n-1) (n*r)
+    
+cfBinomial :: Integer -> Integer -> Integer
+cfBinomial n k
+  | n < k = 0
+  | otherwise = (fatorial n) `div` ((fatorial k) * fatorial (n-k))
 
 -- 08 Faça uma função que calcule o elemento (i,j) do triângulo de pascal.
+triPascal :: Integer -> Integer -> Integer
+triPascal i j = cfBinomial i j
 
 -- Validação Simples
 main = do
@@ -60,7 +81,25 @@ main = do
   let ex4b = ehPrimo 20
   let ex4c = ehPrimo 15
   
-  print (ex4a)
-  print (ex4b)
-  print (ex4c)
+  let ex5a = somaDigitos 19
+  let ex5b = somaDigitos 564894
+  let ex5c = somaDigitos 390
+  
+  let ex6a = persAditiva 8987989
+  let ex6b = persAditiva 3
+  let ex6c = persAditiva 9876
+  
+  let ex7a = cfBinomial 50 3 
+  let ex7b = cfBinomial 11  3
+  let ex7c = cfBinomial 1 2
+  
+  --Linha 5
+  let ex8a = triPascal 5 0
+  let ex8b = triPascal 5 1 
+  let ex8c = triPascal 5 2
+  let ex8d = triPascal 5 3
+  let ex8e = triPascal 5 4
+  let ex8f = triPascal 5 5
+  
+  print (ex8a,ex8b,ex8c,ex8d,ex8e,ex8f)
 
