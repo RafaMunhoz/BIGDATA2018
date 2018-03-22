1bbd8f1c6d6bb2f11be1292af7ce2b3f8804884c
Verum Lumen <rafa.munhoz@terra.com.br>
2018-03-22 10:16:49 -0300

    Update Exercicios4.hs

--- Atividade 01/Exercicios4.hs
+++ Atividade 01/Exercicios4.hs
@@ -1,1 +1,5 @@
+-- 01 Faça uma função que gere uma matriz identidade de tamanho n.
 
+-- 02 Faça uma função que calcule a soma da diagonal principal de uma matriz.
+
+-- 03 Faça uma função que calcule a soma da diagonal secundária de uma matriz.
