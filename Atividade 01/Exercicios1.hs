3c46a3dbabb8442322b20b87d3bb474ca011a46d
Verum Lumen <rafa.munhoz@terra.com.br>
2018-03-22 11:28:29 -0300

    Update Exercicios1.hs

--- Atividade 01/Exercicios1.hs
+++ Atividade 01/Exercicios1.hs
@@ -3,29 +3,29 @@
 2⋅3+5
 2+2⋅3+1
 34+5⋅25+1
--- 02: Faça uma função mult3 x que retorne True caso a entrada seja múltiplo de 3 e False caso contrário.
+-- 02 Faça uma função mult3 x que retorne True caso a entrada seja múltiplo de 3 e False caso contrário.
 
--- 03: Faça uma função mult5 x que retorne True caso a entrada seja múltiplo de 5 e False caso contrário.
+-- 03 Faça uma função mult5 x que retorne True caso a entrada seja múltiplo de 5 e False caso contrário.
 
--- 04: Faça uma função mult35 x que retorne True caso a entrada seja múltiplo de 3 e 5 e False caso contrário.
+-- 04 Faça uma função mult35 x que retorne True caso a entrada seja múltiplo de 3 e 5 e False caso contrário.
 
--- 05: Faça um programa que retorne True caso a entrada seja menor que -1 ou (maior que 1 E múltiplo de 2), e False caso contrário.
+-- 05 Faça um programa que retorne True caso a entrada seja menor que -1 ou (maior que 1 E múltiplo de 2), e False caso contrário.
 
--- 06: Faça uma função que recebe um tipo Integer e retorna ele dividido por 2:
+-- 06 Faça uma função que recebe um tipo Integer e retorna ele dividido por 2:
 
 div2d :: Integer -> Double
 
--- 07: Faa uma função que receba um ângulo a e retorne uma tupla contendo o seno da metade desse ângulo utilizando a identidade:
+-- 07 Faa uma função que receba um ângulo a e retorne uma tupla contendo o seno da metade desse ângulo utilizando a identidade:
 
 sin(x/2)=±1−cos(x)2−−−−−−√
 Exercício 08: Crie uma lista de anos bissextos desde o ano 1 até o atual.
 
--- 09: Encontre os 10 primeiros anos bissextos.
+-- 09 Encontre os 10 primeiros anos bissextos.
 
--- 09: Encontre os 10 últimos anos bissextos (dica: use a função length para determinar o tamanho da lista).
+-- 09 Encontre os 10 últimos anos bissextos (dica: use a função length para determinar o tamanho da lista).
 
--- 10: Crie uma tupla em que o primeiro elemento tem metade dos anos bissextos e o segundo elemento a outra metade.
+-- 10 Crie uma tupla em que o primeiro elemento tem metade dos anos bissextos e o segundo elemento a outra metade.
 
--- 11: Crie um concatenador de strings que concatena duas strings separadas por espaço.
+-- 11 Crie um concatenador de strings que concatena duas strings separadas por espaço.
 
--- 12: Dada a string “0123456789”, crie uma lista com os dígitos em formato Integer.
+-- 12 Dada a string “0123456789”, crie uma lista com os dígitos em formato Integer.
