af0007e0178c3665c94730c65c26a73d76ecc16c
Verum Lumen <rafa.munhoz@terra.com.br>
2018-03-22 22:58:10 -0300

    Update Exercicios3.hs

--- Atividade 01/Exercicios3.hs
+++ Atividade 01/Exercicios3.hs
@@ -1,4 +1,19 @@
 -- 01: Crie uma função divisivel20 x que retorna verdadeiro se x for divisível por todos os números de 1 a 20.
+divisivelN :: Integer -> Integer -> Bool
+divisivelN x n = x `mod` n == 0
+
+divisivelRange :: Integer -> Integer -> Integer -> Bool
+divisivelRange x n count
+  | not(divisivelN x count) = False
+  | count == n = True
+  | otherwise = divisivelRange x n (count+1) 
+
+divisivel20 :: Integer -> Bool
+divisivel20 n = divisivelRange n 20 1
+
+encontra count
+  |divisivel20 count = count
+  |otherwise = encontra (count+1)
 
 -- 02: Crie uma função projectEuler5 que retorna o primeiro número natural que retorna True para a função do exercício anterior. Pense em como reduzir o custo computacional.
 
@@ -13,3 +28,11 @@
 -- 07: Implemente uma função collatzLen x que retorna o tamanho da lista formada pela aplicação repetida de collatz sobre o valor x até que essa chegue no número 1.
 
 -- 08: Encontre o número x entre 1 e 1.000.000 que tem a maior sequência de Collatz. (Project Euler 14)
+
+
+
+-- Validação Simples
+main = do
+  let ex1a = encontra 1
+
+  print (ex1a)
